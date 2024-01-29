f7e1fead987edee8bc7b2874f3edbe5b0e19dee3
MirkoCifelli <ciffo4@hotmail.it>
2024-01-29 17:15:05 +0100

    Inizializzato Progetto

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -14,8 +14,36 @@ export default {
 <template>
     <header>
         HEADER
+
+        <ul>
+            <li>
+                <router-link :to="{ name: 'home' }">
+                        Home
+                    </router-link>
+            </li>
+
+            <li>
+                <router-link :to="{ name: 'about' }">
+                        About
+                    </router-link>
+            </li>
+
+            <li>
+                <router-link :to="{ name: 'contact' }">
+                        Contact
+                    </router-link>
+            </li>
+            <div >
+                <img src="../assets/01-102x102.jpg" alt="">
+            </div>
+            
+        </ul>
     </header>
 </template>
 
 <style lang="scss" scoped>
+img{
+    width: 100px;
+    height: 100px;
+}
 </style>
---
+++ src/pages/ContactPage.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+
+        };
+    }
+}
+</script>
+
+<template>
+    <div>
+        <h1>
+            Contact page
+        </h1>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
